a3f736259e4af3dcdbabd9cc43c7355f0f1a5dd4
Sirius <sirius1@aliyun.com>
2021-04-09 13:15:51 +0800

    add LoadLocaleFromString

--- i18n_test.go
+++ i18n_test.go
@@ -12,8 +12,8 @@ func TestI18N_LoadLocale(t *testing.T) {
 		i18n.LoadLocale("en_US", "./testdata/locale_en_US.ini")
 		i18n.LoadLocale("zh_CN", "./testdata/locale_zh_CN.ini")
 
-		englishLocale, _ := i18n.getLocale("en_US")
-		chineseLocale, _ := i18n.getLocale("zh_CN")
+		englishLocale, _ := i18n.GetLocale("en_US")
+		chineseLocale, _ := i18n.GetLocale("zh_CN")
 
 		englishMessage := englishLocale.GetMessage("terminal.print")
 		chineseMessage := chineseLocale.GetMessage("terminal.print")
